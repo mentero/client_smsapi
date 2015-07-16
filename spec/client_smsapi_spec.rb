0d61dc4bb432411e60b6a04bdfc970fe07f2903a
Artur Debski <xmentero@gmail.com>
2015-07-16 20:39:13 +0200

    Add simplecov

--- spec/client_smsapi_spec.rb
+++ spec/client_smsapi_spec.rb
@@ -1,5 +1,3 @@
-require 'spec_helper'
-
 RSpec.describe SMSApi do
   it 'has a version number' do
     expect(SMSApi::VERSION).not_to be nil
--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,32 @@
+require 'simplecov'
+SimpleCov.start
+
 $LOAD_PATH.unshift File.expand_path('../../lib', __FILE__)
 require 'client_smsapi'
+
+RSpec.configure do |config|
+  config.expect_with :rspec do |expectations|
+    # This option will default to `true` in RSpec 4. It makes the `description`
+    # and `failure_message` of custom matchers include text for helper methods
+    # defined using `chain`, e.g.:
+    #     be_bigger_than(2).and_smaller_than(4).description
+    #     # => "be bigger than 2 and smaller than 4"
+    # ...rather than:
+    #     # => "be bigger than 2"
+    expectations.include_chain_clauses_in_custom_matcher_descriptions = true
+  end
+
+  config.mock_with :rspec do |mocks|
+    # Prevents you from mocking or stubbing a method that does not exist on
+    # a real object. This is generally recommended, and will default to
+    # `true` in RSpec 4.
+    mocks.verify_partial_doubles = true
+  end
+
+  config.disable_monkey_patching!
+  # config.warnings = true
+  config.default_formatter = 'doc' if config.files_to_run.one?
+  config.profile_examples = 10
+  config.order = :random
+  Kernel.srand config.seed
+end
